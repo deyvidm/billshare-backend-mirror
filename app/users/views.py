abfd936f84d471c031d0ba681eacc656b0ed22cd
Deyvid <deyvidmitkov@gmail.com>
2017-10-05 11:58:24 -0400

    http->json; double -> single quotes; removed admin.py

--- app/users/views.py
+++ app/users/views.py
@@ -1,11 +1,15 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from django.views import View
-from django.http import HttpResponse
+from django.http import JsonResponse
 
 
 class Users(View):
-    testMessage = "this is a test"
 
     def get(self, request):
-        return HttpResponse(self.testMessage)
+        data = {
+                'success': True,
+                'data': 'this is some test data'
+                }
+
+        return JsonResponse(data)
