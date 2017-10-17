0344a00e1b1cb3af06b66e719b73ddd031ddad07
jakebesworth <jakebesworth@gmail.com>
2017-10-16 20:29:11 -0400

    Squashed migrations

--- app/user/models.py
+++ app/user/models.py
@@ -1,5 +1,14 @@
-from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 from django.db import models
+
+
+class UserManager(BaseUserManager):
+    def _create_user(self, email, password, **extra_fields):
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
 
 
 class User(AbstractBaseUser):
