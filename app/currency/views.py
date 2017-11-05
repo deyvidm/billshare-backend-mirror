ac2e4d5a156dacacc0f3113b5c129d50deefb192
jakebesworth <jakebesworth@gmail.com>
2017-11-05 12:54:12 -0500

    Added Fixer.io API service

--- app/currency/views.py
+++ app/currency/views.py
@@ -2,7 +2,7 @@ from django.utils.decorators import method_decorator
 from django.views import View
 from django.views.decorators.csrf import csrf_exempt
 
-from app.currency.services import CurrencyService
+from app.currency.services import FixerCurrencyService
 from app.response.services import ResponseService
 
 
@@ -10,7 +10,28 @@ from app.response.services import ResponseService
 class CurrencyCodesView(View):
 
     response_service = ResponseService()
-    currency_service = CurrencyService()
+    fixer_currency_service = FixerCurrencyService()
 
     def get(self, request):
-        return self.response_service.success(self.currency_service.get_currency_codes())
+
+        currency_codes = self.fixer_currency_service.get_currency_codes()
+
+        if currency_codes:
+            return self.response_service.success(currency_codes)
+
+        return self.response_service.failure({'error': 'Could not provide currency codes'})
+
+
+@method_decorator(csrf_exempt, name='dispatch')
+class CurrencyView(View):
+
+    response_service = ResponseService()
+    fixer_currency_service = FixerCurrencyService()
+
+    def get(self, request):
+        currency_codes = self.fixer_currency_service.get_currency_code_rates()
+
+        if currency_codes:
+            return self.response_service.success(currency_codes)
+
+        return self.response_service.failure({'error': 'Could not provide currency codes'})
