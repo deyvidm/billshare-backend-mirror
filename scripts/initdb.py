97ccd1d705ccb135e479913fcf7788abee0b95a2
Deyvid <deyvidmitkov@gmail.com>
2017-11-27 21:25:59 -0500

    uncommented

--- scripts/initdb.py
+++ scripts/initdb.py
@@ -49,46 +49,46 @@ group_1 = create_group("frozen cunts", "tormund@test.ca", ["jon@test.ca", "sando
 # group_1 = 1
 
 
-# trans_id = create_transaction({
-#     "total": 100,
-#     "currency_code": "CAD",
-#     "label": "User 1 owes User 2",
-#     "group": group_1,
-#     "creator": user_1,
-#     "split_type": "money",
-#     "user_shares": [
-#         {
-#             "user": user_1,
-#             "owes": 40,
-#             "paid": 30
-#         },
-#         {
-#             "user": user_2,
-#             "owes": 60,
-#             "paid": 70
-#         },
-#     ]
-# })
-# print "transaction with id {} successfully created".format(trans_id)
-#
-# trans_id = create_transaction({
-#     "total": 100,
-#     "currency_code": "CAD",
-#     "label": "User 2 owes User 3",
-#     "group": group_1,
-#     "creator": user_2,
-#     "split_type": "money",
-#     "user_shares": [
-#         {
-#             "user": user_2,
-#             "owes": 90,
-#             "paid": 30
-#         },
-#         {
-#             "user": user_3,
-#             "owes": 10,
-#             "paid": 70
-#         },
-#     ]
-# })
-# print "transaction with id {} successfully created".format(trans_id)
+trans_id = create_transaction({
+    "total": 100,
+    "currency_code": "CAD",
+    "label": "User 1 owes User 2",
+    "group": group_1,
+    "creator": user_1,
+    "split_type": "money",
+    "user_shares": [
+        {
+            "user": user_1,
+            "owes": 40,
+            "paid": 30
+        },
+        {
+            "user": user_2,
+            "owes": 60,
+            "paid": 70
+        },
+    ]
+})
+print "transaction with id {} successfully created".format(trans_id)
+
+trans_id = create_transaction({
+    "total": 100,
+    "currency_code": "CAD",
+    "label": "User 2 owes User 3",
+    "group": group_1,
+    "creator": user_2,
+    "split_type": "money",
+    "user_shares": [
+        {
+            "user": user_2,
+            "owes": 90,
+            "paid": 30
+        },
+        {
+            "user": user_3,
+            "owes": 10,
+            "paid": 70
+        },
+    ]
+})
+print "transaction with id {} successfully created".format(trans_id)
